9d17e60e2d28de9f6f54ee491b3f4f44d2478a22
Andreas Helgegren <andreas.helgegren@gmail.com>
2014-08-22 15:55:49 +0200

    Create CacheJob.php

---
+++ CacheJob.php
@@ -0,0 +1,19 @@
+<?php
+
+	namespace ProspectEye\CacheJob;
+	
+	class CacheJob() {
+	
+		function __construct() {
+		
+		}
+		
+		function run() {
+		
+			// run the job to update which visits should be stored in the cache
+			
+			// this job could be a cronjob and be scheduled as pleased
+			
+		}
+	
+	}
