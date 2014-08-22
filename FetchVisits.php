0583c7e9d85254561cefc8d414ac8b8133f621f2
Andreas Helgegren <andreas.helgegren@gmail.com>
2014-08-22 15:51:29 +0200

    Create FetchVisits.php

---
+++ FetchVisits.php
@@ -0,0 +1,24 @@
+<?php
+
+	namespace ProspectEye\FetchVisits;
+	
+	class FetchVisits {
+	
+		protected $_cache;
+	
+		function __construct() {
+			$this->_cache = NULL;
+		}
+		
+		function getVisits($accountId) {
+		
+			// check if this accountId should be cached
+			
+			// check if we could fetch from cache
+			
+			// return result
+			
+		}
+	
+	}
+	
