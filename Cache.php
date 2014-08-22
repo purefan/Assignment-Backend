7fb8d97f9aaf0865a24b8131075cdaa7a2ea0184
Andreas Helgegren <andreas.helgegren@gmail.com>
2014-08-22 14:57:30 +0200

    Cache.php created

---
+++ Cache.php
@@ -0,0 +1,28 @@
+<?php
+
+	namespace ProspectEye\Cache;
+
+	class Cache {
+		
+		function __construct() {
+		
+		}
+		
+		function getVisitsByAccountId($accountId) {
+		
+		}
+		
+		function refreshCacheByAccountId($accountId) {
+		
+		}
+		
+		function getCachedVisitsByAccountId($accountId) {
+		
+		}
+		
+		function getUncachedVisitsByAccountId($accountId) {
+		
+		}
+	}
+
+?>
